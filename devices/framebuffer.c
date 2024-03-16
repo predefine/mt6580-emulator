8f77b26c14684bad65a6cb5a517bbbfbda4bffb4
predefine <predefine@yandex.ru>
2024-03-17 00:52:44 +0700

    framebuffer driver optimisation

--- devices/framebuffer.c
+++ devices/framebuffer.c
@@ -1,3 +1,4 @@
+#include <log.h>
 #include <devices.h>
 #include <unicorn/unicorn.h>
 #include <SDL2/SDL.h>
@@ -16,7 +17,8 @@ int framebuffer_update(void* arg);
 SDL_Thread* update_thread;
 
 void framebuffer_init(uc_engine* uc, void* devptr){
-    device* dev = devptr;
+    (void)uc;
+    (void)devptr;
     window = SDL_CreateWindow("MT6580 Emulator",
                               SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
                               WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
@@ -38,6 +40,7 @@ void framebuffer_init(uc_engine* uc, void* devptr){
 }
 
 int framebuffer_update(void* arg){
+    (void)arg;
     while(update){
         SDL_RenderCopy(renderer, fbtext, NULL, NULL);
         SDL_RenderPresent(renderer);
@@ -47,18 +50,30 @@ int framebuffer_update(void* arg){
 }
 
 void framebuffer_callback(uc_engine* uc, uc_mem_type type, uint64_t address, int size, long valuel, void* user_data){
+    (void)uc;
+    if(type != UC_MEM_WRITE) return;
+    (void)size;
+    (void)user_data;
     uint32_t value = valuel;
     device* dev = user_data;
     uint32_t screen_pos = address - dev->address;
     void* pixels;
     int pitch;
     SDL_LockTexture(fbtext, NULL, &pixels, &pitch);
-    *((uint32_t*)&pixels[screen_pos]) = value;
+    if(size == 4){
+        ((uint32_t*)pixels)[screen_pos >> 2] = value;
+    } else if (size == 1){
+        ((char*)pixels)[screen_pos] = value;
+    } else {
+        PANIC_MSG("not supported size %d!\n", size);
+    }
     SDL_UnlockTexture(fbtext);
     return;
 }
 
 void framebuffer_exit(uc_engine* uc, void* devptr){
+    (void)uc;
+    (void)devptr;
     update = 0;
     SDL_WaitThread(update_thread, NULL);
 }
