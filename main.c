e38e4c8e6caa946032b3103e60a21dcc989d8e11
predefine <predefine@yandex.ru>
2024-03-17 00:53:18 +0700

    split "main" into functions

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <unicorn/unicorn.h>
 #include <unicorn/arm.h>
+#include <log.h>
 // #include <udbserver.h>
 #include <stdio.h>
 #include <signal.h>
@@ -9,19 +10,13 @@
 
 #define BOOTVECTOR_RESET KERNEL_OFFSET
 
-// #define BOOTROM_OFFSET 0
-// #define BOOTROM_SIZE 0x100000
-// #define BOOTROM_FILE "brom_6580.bin"
 
-// #define PRELOADER_OFFSET (0x201000)
-// #define PRELOADER_SIZE (128*1024)
-// #define PRELOADER_FILE "/mnt/alps/alps/out/target/product/pmt3151/obj/PRELOADER_OBJ/bin/preloader_pmt3151_LINKED.bin"
-
-#define UBOOT_OFFSET 0x81e00000-0x200
+#define UBOOT_OFFSET (0x81e00000-0x200)
 #define UBOOT_SIZE 0x800000
 #define UBOOT_FILE "../u-boot/out/u-boot-mtk.bin"
 
 #define KERNEL_OFFSET 0x80008000
+#define KERNEL_DTB_OFFSET (KERNEL_OFFSET + 0x8000000)
 #define KERNEL_SIZE 0x800000
 #define KERNEL_FILE "kernel"
 
@@ -48,12 +43,9 @@ void mem_info(uc_engine* uc, uc_mem_type type, uint64_t address, int size, long 
     (void)type;
     if(type == UC_MEM_READ)
         uc_mem_read(uc, address, &value, size);
-    //if(address < RAM_OFFSET || address > RAM_OFFSET + RAM_SIZE - 1)){
-    // if((address < 0x11005000 || address > 0x11007000) && (address < RAM_OFFSET ) && address > 64*1024+SRAM_OFFSET){
-        uint32_t r_pc;
-        uc_reg_read(uc, UC_ARM_REG_PC, &r_pc);
-        printf("INFO! detected %s at 0x%lx-0x%lx with value=0x%lx at pc=0x%x\n", type == UC_MEM_READ ? "read" : "write", address, address+size, value, r_pc);
-    // }
+    uint32_t r_pc;
+    uc_reg_read(uc, UC_ARM_REG_PC, &r_pc);
+    printf("INFO! detected %s at 0x%lx-0x%lx with value=0x%lx at pc=0x%x\n", type == UC_MEM_READ ? "read" : "write", address, address+size, value, r_pc);
 }
 #endif
 
@@ -67,202 +59,16 @@ void emu_exit(){
     printf("Emulator terminating...\n");
     for(int i = 0; devices[i] != NULL; i++){
         device* dev = devices[i];
-        if(dev->exit){
-            printf("Exiting from %s!\n", dev->name);
+        if(dev->exit)
             dev->exit(engine, (void*)dev);
-        }
     }
-    printf("bye bye!\n");
     exit(0);
 }
 
-int main(){
-    uc_err err;
-
-    if(uc_open(UC_ARCH_ARM, UC_MODE_LITTLE_ENDIAN | UC_MODE_THUMB, &engine) != UC_ERR_OK){
-        printf("uc_open failed :(\n");
-        return -1;
-    }
-    if(uc_ctl_set_cpu_model(engine, UC_CPU_ARM_CORTEX_A7) != UC_ERR_OK){
-        printf("uc_ctl_set_cpu_model failed :(\n");
-        return -1;
-    }
-
-    // 64 kb sram
-    err = uc_mem_map(engine, SRAM_OFFSET, SRAM_SIZE, UC_PROT_ALL);
-    if (err) {
-        printf("Failed map on-chip sram with error returned %u: %s\n", err, uc_strerror(err));
-        return -1;
-    }
-
-    // 128 mb ram
-    err = uc_mem_map(engine, RAM_OFFSET, RAM_SIZE, UC_PROT_ALL);
-    if (err) {
-        printf("Failed map ram with error returned %u: %s\n", err, uc_strerror(err));
-        return -1;
-    }
-    printf("ram mapped!\n");
-
-
-    int fd;
-
-    //Bootrom
-    // char *bootrom = (char*)malloc(BOOTROM_SIZE*sizeof(char));
-    // int fd;
-    // if ((fd = open(BOOTROM_FILE, O_RDONLY)) < 0){
-    //     printf("bootrom open file failed :(");
-    //     return -1;
-    // }
-    // int bootrom_size = 0;
-    // if ((bootrom_size = read(fd, bootrom, BOOTROM_SIZE)) < 1){
-    //     printf("bootrom read failed :(");
-    //     return -1;
-    // }
-    // printf("brom size: 0x%x\n", bootrom_size);
-    // close(fd);
-    // err = uc_mem_map(engine, BOOTROM_OFFSET, bootrom_size, UC_PROT_ALL);
-    // if (err) {
-    //     printf("Failed map bootrom with error returned %u: %s\n", err, uc_strerror(err));
-    //     return -1;
-    // }
-    // printf("bootrom memory mapped!\n");
-    // err = uc_mem_write(engine, BOOTROM_OFFSET, bootrom, bootrom_size);
-    // if (err) {
-    //     printf("Failed write bootrom with error returned %u: %s\n", err, uc_strerror(err));
-    //     return -1;
-    // }
-    // printf("bootrom memory writed!\n");
-    // free(bootrom);
-
-    // Preloader
-    // char *preloader = (char*)malloc(PRELOADER_SIZE*sizeof(char));
-    // if ((fd = open(PRELOADER_FILE, O_RDONLY)) < 0){
-    //     printf("preloader open file failed :(");
-    //     return -1;
-    // }
-    // if (read(fd, preloader, PRELOADER_SIZE) < 1){
-    //     printf("preloader read failed :(");
-    //     return -1;
-    // }
-    // close(fd);
-    // err = uc_mem_map(engine, PRELOADER_OFFSET, PRELOADER_SIZE, UC_PROT_ALL);
-    // if (err) {
-    //     printf("Failed map preloader with error returned %u: %s\n", err, uc_strerror(err));
-    //     return -1;
-    // }
-    // printf("preloader memory mapped!\n");
-    // err = uc_mem_write(engine, PRELOADER_OFFSET, preloader, PRELOADER_SIZE);
-    // if (err) {
-    //     printf("Failed write preloader with error returned %u: %s\n", err, uc_strerror(err));
-    //     return -1;
-    // }
-    // printf("preloader memory writed!\n");
-    // free(preloader);
-
-    // Uboot
-    char *uboot = (char*)malloc(UBOOT_SIZE*sizeof(char));
-    if ((fd = open(UBOOT_FILE, O_RDONLY)) < 0){
-        printf("uboot open file failed :(\n");
-        return -1;
-    }
-    if (read(fd, uboot, UBOOT_SIZE) < 1){
-        printf("uboot read failed :(\n");
-        return -1;
-    }
-    close(fd);
-    err = uc_mem_write(engine, UBOOT_OFFSET, uboot, UBOOT_SIZE);
-    if (err) {
-        printf("Failed write uboot with error returned %u: %s\n", err, uc_strerror(err));
-        return -1;
-    }
-    printf("uboot memory writed!\n");
-    free(uboot);
-
-    char *kernel = (char*)malloc(KERNEL_SIZE*sizeof(char));
-    if ((fd = open(KERNEL_FILE, O_RDONLY)) < 0){
-        printf("kernel open file failed :(\n");
-        return -1;
-    }
-    if (read(fd, kernel, KERNEL_SIZE) < 1){
-        printf("kernel read failed :(\n");
-        return -1;
-    }
-    close(fd);
-    err = uc_mem_write(engine, KERNEL_OFFSET, kernel, KERNEL_SIZE);
-    if (err) {
-        printf("Failed write kernel with error returned %u: %s\n", err, uc_strerror(err));
-        return -1;
-    }
-    printf("kernel memory writed!\n");
-    free(kernel);
-
-    uc_hook mem_unmapped_read_hook;
-    err = uc_hook_add(engine, &mem_unmapped_read_hook,
-                UC_HOOK_MEM_UNMAPPED, mem_read_unmapped, NULL, 1, 0);
-    if (err){
-         printf("unmapped read&write hook add failed with error %u %s!\n", err, uc_strerror(err));
-    }
-
-#ifdef DEBUG_MEM
-    uc_hook mem_info_hook;
-    err = uc_hook_add(engine, &mem_info_hook,
-                UC_HOOK_MEM_READ | UC_HOOK_MEM_WRITE, mem_info, NULL, 1, 0);
-    if (err){
-         printf("mem read&write hook add failed with error %u %s!\n", err, uc_strerror(err));
-    }
-#endif
-
-    printf("adding devices...\n");
-    for(int i = 0; devices[i] != NULL; i++){
-        device* dev = devices[i];
-        printf("adding device %d: %s...\n", i, dev->name);
-        err = uc_mem_map(engine, dev->address, dev->size, UC_PROT_READ | UC_PROT_WRITE);
-        if(err && (dev->address < RAM_OFFSET)){
-            printf("Adding device %d: mapping memory failed with error %u: %s!\n", i, err, uc_strerror(err));
-            continue;
-        }
-        if(dev->callback){
-            printf("Adding device %d callback...\n", i);
-            // if(dev->hook == NULL)
-            uc_hook* hook = malloc(sizeof(uc_hook));
-            err = uc_hook_add(engine, hook, UC_HOOK_MEM_READ | UC_HOOK_MEM_WRITE, dev->callback, dev, dev->address,
-                    dev->address + dev->size - 1);
-            if (err){
-                printf("Adding device %d: adding hook failed with error %u!\n", i, err/*uc_strerror(err)*/);
-                continue;
-            }
-        }
-        if(dev->init)
-            dev->init(engine, (void*)dev);
-    }
-    printf("Devices added!\n");
-
-    // printf("adding udbserver hook... ");
-    // udbserver(engine, 1235, BOOTVECTOR_RESET);
-    // printf("Done!\n");
-
-    printf("Fixing kernel arguments...");
-    uint32_t r_fdt_offset;
-    uint32_t fdt_new_offset = KERNEL_OFFSET + 0xe000000;
-    uint32_t kernel_load_address, kernel_end_address;
-    uc_mem_read(engine, KERNEL_OFFSET + 0x28, &kernel_load_address, sizeof(kernel_load_address));
-    uc_mem_read(engine, KERNEL_OFFSET + 0x2c, &kernel_end_address, sizeof(kernel_end_address));
-    uint32_t kernel_size = kernel_end_address - kernel_load_address;
-    r_fdt_offset = KERNEL_OFFSET + kernel_size;
-    uint32_t dtb_size;
-    uc_mem_read(engine, r_fdt_offset+0x4, &dtb_size, sizeof(dtb_size));
-    dtb_size = CPU_TO_FDT32(dtb_size);
-    uint32_t buffer;
-    for(uint32_t i = 0; i < dtb_size; i += sizeof(buffer)){
-        uc_mem_read(engine, r_fdt_offset + i, &buffer, sizeof(buffer));
-        uc_mem_write(engine, fdt_new_offset + i, &buffer, sizeof(buffer));
-    }
-    uc_reg_write(engine, UC_ARM_REG_R2, &fdt_new_offset);
-    printf("Done!\n");
-
+void emu_start(uint32_t start){
     printf("Starting emulator...\n");
     signal(SIGINT, emu_exit);
-    err=uc_emu_start(engine, BOOTVECTOR_RESET, 0xffffffff, 0, 0);
+    uc_err err = uc_emu_start(engine, start, 0xffffffff, 0, 0);
     if (err) {
         printf("Failed on uc_emu_start() with error returned %u: %s\n",
         err, uc_strerror(err));
@@ -271,7 +77,122 @@ int main(){
     uint32_t r_pc;
     uc_reg_read(engine, UC_ARM_REG_PC, &r_pc);
     printf("PC: 0x%x\n", r_pc);
-    // printf("Total ticks: %d\n", timer_gpt_ticks);
+}
+
+void emu_init(){
+    if(uc_open(UC_ARCH_ARM, UC_MODE_LITTLE_ENDIAN | UC_MODE_THUMB, &engine) != UC_ERR_OK){
+        printf("uc_open failed :(\n");
+        exit(-1);
+    }
+    if(uc_ctl_set_cpu_model(engine, UC_CPU_ARM_CORTEX_A7) != UC_ERR_OK){
+        printf("uc_ctl_set_cpu_model failed :(\n");
+        exit(-1);
+    }
+}
+
+void map_mem(uint64_t address, uint64_t size, char ignore_err){
+    uc_err err = uc_mem_map(engine, address, size, UC_PROT_ALL);
+    if (err && !ignore_err)
+        PANIC_MSG("Failed map memory at 0x%lx, size 0x%lx with error returned %u: %s\n", address, size, err, uc_strerror(err));
+}
+
+void load_file(char* filename, uint64_t address, uint64_t size){
+    char *data = (char*)malloc(size);
+    int fd;
+    if ((fd = open(filename, O_RDONLY)) < 0)
+        PANIC_MSG("load_file: open file failed :(\n");
+    if (read(fd, data, size) < 1)
+        PANIC_MSG("load_file: read failed :(\n");
+    close(fd);
+    uc_err err = uc_mem_write(engine, address, data, size);
+    if (err)
+        PANIC_MSG("load_file: Failed write memory with error returned %u: %s\n", err, uc_strerror(err));
+    DEBUG_MSG("load_file: memory writed!\n");
+    free(data);
+}
+
+void map_mem_and_load_file(char* filename, uint64_t address, uint64_t size){
+    map_mem(address, size, 1);
+    load_file(filename, address, size);
+}
+
+void devices_probe(){
+    for(int i = 0; devices[i] != NULL; i++){
+        device* dev = devices[i];
+        printf("adding device %d: %s...\n", i, dev->name);
+        map_mem(dev->address, dev->size, 1);
+        if(dev->callback){
+            printf("Adding device %d callback...\n", i);
+            uc_hook* hook = malloc(sizeof(uc_hook));
+            uc_err err = uc_hook_add(engine, hook, UC_HOOK_MEM_READ | UC_HOOK_MEM_WRITE, dev->callback, dev, dev->address,
+                    dev->address + dev->size - 1);
+            if (err){
+                printf("Adding device %d: adding hook failed with error %u!\n", i, err);
+            }
+        }
+        if(dev->init)
+            dev->init(engine, (void*)dev);
+    }
+}
+
+void add_hooks(){
+    uc_hook mem_unmapped_read_hook;
+    uc_err err = uc_hook_add(engine, &mem_unmapped_read_hook,
+                UC_HOOK_MEM_UNMAPPED, mem_read_unmapped, NULL, 1, 0);
+    if (err){
+         printf("unmapped read&write hook add failed with error %u %s!\n", err, uc_strerror(err));
+    }
+
+    #ifdef DEBUG_MEM
+        uc_hook mem_info_hook;
+        err = uc_hook_add(engine, &mem_info_hook,
+                    UC_HOOK_MEM_READ | UC_HOOK_MEM_WRITE, mem_info, NULL, 1, 0);
+        if (err){
+            printf("mem read&write hook add failed with error %u %s!\n", err, uc_strerror(err));
+        }
+    #endif
+}
+
+void find_dtb_in_kernel(uint32_t offset, uint32_t* dtb_offset, uint32_t* dtb_size){
+    uint32_t kernel_load_address, kernel_end_address;
+    uc_mem_read(engine, offset + 0x28, &kernel_load_address, sizeof(kernel_load_address));
+    uc_mem_read(engine, offset + 0x2c, &kernel_end_address, sizeof(kernel_end_address));
+    uint32_t kernel_size = kernel_end_address - kernel_load_address;
+    *dtb_offset = offset + kernel_size;
+    uint32_t _dtb_size;
+    uc_mem_read(engine, (*dtb_offset)+0x4, &_dtb_size, sizeof(_dtb_size));
+    *dtb_size = CPU_TO_FDT32(_dtb_size);
+}
+
+void copy_mem(uint32_t offset, uint32_t new_offset, uint32_t size){
+    uint32_t buffer;
+    for(uint32_t i = 0; i < size; i += sizeof(buffer)){
+        uc_mem_read(engine, offset + i, &buffer, sizeof(buffer));
+        uc_mem_write(engine, new_offset + i, &buffer, sizeof(buffer));
+    }
+}
+
+int main(){
+    emu_init();
+
+    add_hooks();
+    // 64 kb SRAM
+    map_mem(SRAM_OFFSET, SRAM_SIZE, 0);
+    // RAM
+    map_mem(RAM_OFFSET, RAM_SIZE, 0);
+
+    load_file(UBOOT_FILE, UBOOT_OFFSET, UBOOT_SIZE);
+    load_file(KERNEL_FILE, KERNEL_OFFSET, KERNEL_SIZE);
+
+    devices_probe();
+
+    uint32_t dtb_offset, dtb_size, remap_offset;
+    remap_offset = KERNEL_DTB_OFFSET;
+    find_dtb_in_kernel(KERNEL_OFFSET, &dtb_offset, &dtb_size);
+    copy_mem(dtb_offset, remap_offset, dtb_size);
+    uc_reg_write(engine, UC_ARM_REG_R2, &remap_offset);
+
+    emu_start(BOOTVECTOR_RESET);
 
     return 0;
 }
