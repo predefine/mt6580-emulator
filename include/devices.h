5441a0bff0461f728b910a3df1213db832b397d0
predefine <predefine@yandex.ru>
2024-03-22 02:17:22 +0700

    OOPS, forgot add include/devices.h

--- include/devices.h
+++ include/devices.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <unicorn/unicorn.h>
 
-#define FRAMEBUFFER_ADDRESS 0xbf000000
+#define FRAMEBUFFER_ADDRESS 0xbf100000
 
 typedef struct {
     uint64_t address;
@@ -15,4 +15,20 @@ typedef struct {
     void (*exit)(uc_engine* uc, void* dev);
 } device;
 
-extern device *devices[];
+typedef struct _devices_list{
+    device* this;
+    struct _devices_list* next;
+    size_t next_count;
+    size_t total;
+} devices_list;
+
+const devices_list* get_devices();
+void add_device(char* name, device dev);
+
+#define DEVICES_INIT_PRIOR 101
+#define DEVICES_APPEND_PRIOR 102
+
+#define DEVICE(name, ...) \
+    static void __attribute__((constructor(DEVICES_APPEND_PRIOR))) devices_init_dev_##name (void) { \
+        add_device(#name, (device)__VA_ARGS__); \
+    }
